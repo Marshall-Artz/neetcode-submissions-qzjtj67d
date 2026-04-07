03f83042e81235349bbdd879b0443903ffb28c40
Marshall-Artz <90860127+Marshall-Artz@users.noreply.github.com>
2026-04-06 21:18:03 -0500

    Add: single-threaded-cpu - submission-1

---
+++ Data Structures & Algorithms/single-threaded-cpu/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def getOrder(self, tasks: List[List[int]]) -> List[int]:
+        if not tasks:
+            return []
+
+        for i in range(len(tasks)):
+            tasks[i].append(i)
+        
+        heapq.heapify(tasks)
+
+        res  = []
+        heap = []
+        time = tasks[0][0]
+
+        while tasks or heap:
+            while tasks and tasks[0][0] <= time:
+                enqTime, procTime, index = heapq.heappop(tasks)
+                heapq.heappush(heap, (procTime, index))
+
+            if heap:
+                procTime, index = heapq.heappop(heap)
+                time += procTime
+                res.append(index)
+            else:
+                time = tasks[0][0]
+                continue
+
+        return res
