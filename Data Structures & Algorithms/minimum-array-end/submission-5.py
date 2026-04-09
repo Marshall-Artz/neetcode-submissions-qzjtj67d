3f470e0713ac68eab4d3671ba7958c17ebb1fde8
Marshall-Artz <90860127+Marshall-Artz@users.noreply.github.com>
2026-04-09 11:21:03 -0500

    Add: minimum-array-end - submission-5

---
+++ Data Structures & Algorithms/minimum-array-end/submission-5.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minEnd(self, n: int, x: int) -> int:
+
+        n   = n - 1
+        res = x
+        i   = 0
+        while n > 0:
+            cur = 1 << i
+
+            if cur & x:
+                i += 1
+                continue
+            
+            if n & 1 == 1:
+                res = res | cur
+            
+            n = n >> 1
+            i += 1
+            
+        return res
