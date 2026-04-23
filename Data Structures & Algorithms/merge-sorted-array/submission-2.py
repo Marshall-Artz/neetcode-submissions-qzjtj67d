9831772c18f52c94bb6252dc954cc6257c28a569
Marshall-Artz <90860127+Marshall-Artz@users.noreply.github.com>
2026-04-23 12:45:51 -0500

    Add: merge-sorted-array - submission-2

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        if n == 0:
+            return
+
+        heapq.heapify(nums2)
+
+        for i in range(len(nums1) - n):
+            heapq.heappush(nums2, nums1[i])
+            nums1[i] = heapq.heappop(nums2)
+
+        for i in range(n):
+            nums1[m + i] = heapq.heappop(nums2)
