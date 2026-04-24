89db07a05c792f008ce51fa2c9b8f91046fbba57
Marshall-Artz <90860127+Marshall-Artz@users.noreply.github.com>
2026-04-24 10:21:39 -0500

    Add: minimum-interval-including-query - submission-4

---
+++ Data Structures & Algorithms/minimum-interval-including-query/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
+        intervals.sort()
+        minHeap = []
+
+        res = {}
+        
+        i = 0
+        for query in sorted(queries):
+            while i < len(intervals) and intervals[i][0] <= query:
+                left, right = intervals[i]
+                length = right - left + 1
+                heapq.heappush(minHeap, (length, right))
+                i += 1
+            
+            while minHeap and minHeap[0][1] < query:
+                heapq.heappop(minHeap)
+            
+            res[query] = minHeap[0][0] if minHeap else -1
+
+        return [res[query] for query in queries]
