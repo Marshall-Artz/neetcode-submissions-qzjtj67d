e31bfa509eed8a3eaa4a33c785b4b27e5578bdf6
Marshall-Artz <90860127+Marshall-Artz@users.noreply.github.com>
2026-05-08 12:52:16 -0500

    Add: house-robber - submission-5

---
+++ Data Structures & Algorithms/house-robber/submission-5.py
@@ -0,0 +1,18 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+        mem = {}
+        def dfs(pos: int) -> int:
+            if pos >= len(nums):
+                return 0
+            if pos in mem:
+                return mem[pos]
+            
+            # Rob current house, or rob next house
+            mem[pos] = max(
+                nums[pos] + dfs(pos + 2),
+                dfs(pos + 1)
+            )
+            return mem[pos]
+        
+        return dfs(0)
