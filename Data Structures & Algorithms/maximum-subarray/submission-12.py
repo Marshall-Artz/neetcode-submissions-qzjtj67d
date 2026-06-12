61808a791fb986350d41d8ef5aebd1ef69e88eb2
Marshall-Artz <90860127+Marshall-Artz@users.noreply.github.com>
2026-06-12 12:34:30 -0500

    Add: maximum-subarray - submission-12

---
+++ Data Structures & Algorithms/maximum-subarray/submission-12.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        
+        mem = {}
+        def dfs(i: int, value: int) -> int:
+            if (i, value) in mem:
+                return mem[(i, value)]
+            if i >= len(nums):
+                return value
+            
+            # Keep going with current array or stop and start new array:
+            mem[(i, value)] = max(
+                value,
+                dfs(i + 1, value + nums[i]),
+                dfs(i + 1, nums[i])
+            )
+
+            return mem[(i, value)]
+        
+        return dfs(1,nums[0])
